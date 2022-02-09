ce835dfc11fd26d285893ece05bfe08429822d56
Luiza <luiza.c.andrade@gmail.com>
2022-02-08 19:07:06 -0500

    modularize

---
+++ load.js
@@ -0,0 +1,32 @@
+
+jQuery(document).ready(function(){
+    jQuery("#nav").load("resources/navigation.html");
+});
+
+jQuery(document).ready(function(){
+    jQuery("#pub").load("resources/publications.html");
+});
+
+jQuery(document).ready(function(){
+    jQuery("#blogs").load("resources/blogs.html");
+});
+
+jQuery(document).ready(function(){
+    jQuery("#cv-exp").load("resources/cv-exp.html");
+});
+
+jQuery(document).ready(function(){
+    jQuery("#cv-header").load("resources/cv-header.html");
+});
+
+jQuery(document).ready(function(){
+    jQuery("#cv-skills").load("resources/cv-skills.html");
+});
+
+jQuery(document).ready(function(){
+    jQuery("#pub").load("resources/pub.html");
+});
+
+jQuery(document).ready(function(){
+    jQuery("#blogs").load("resources/blogs.html");
+});
